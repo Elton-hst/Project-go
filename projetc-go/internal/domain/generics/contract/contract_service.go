7beecc304c34dd0cb34fd3cf46613e252dd03265
ELTON HENRIQUE <eltonhst34@gmail.com>
2024-04-18 12:44:51 -0300

    Delete projetc-go directory

--- projetc-go/internal/domain/generics/contract/contract_service.go
+++
@@ -1,9 +0,0 @@
-package contract
-
-type ContractService[T any] interface {
-	Create(entity *T) (*T, error)
-	FindById(id string) (*T, error)
-	FindAll() ([]*T, error)
-	Update(entity *T) (*T, error)
-	DeleteById(id string) error
-}
